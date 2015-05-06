6ff3e0f41eb574b4b717cb019920fd1d54bd0281
fredyramix <freddyramix@gmail.com>
2015-05-06 01:13:26 -0500

    Falta modificar sigma

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 __author__ = 'Freddy'
 import random
 
-def Sol_ale(min,max,tam,vector):
+def Sol_ale(min,max,tam):
+    vector = []
     for i in range(tam):
         vector.append(random.uniform(min,max))
     return vector
@@ -23,34 +24,55 @@ def Mutar(ale,n):
         xi.append(ale[i]+n[i])
     return xi
 
+def imprimir(g,v,a,e):
+    print "Generacion:"+str(g)+"\tVector:"+str(v)+"\tAptitud:"+str(a)+"\tExitos:"+str(e)
+
+def exitos(eo,ei,e):
+    if len(ei)==0:
+        return e
+    elif ei<eo:
+        return e+1
+
+def comparar(eo,ei,E,xi,ale):
+    if eo < ei:
+        return E,eo,ale
+    else:
+        return E+1,ei,xi
+def Modificar_Sigma():
+    pass
 
 def main():
     x=0
-    #Q = Sigma
-    Q=0.1
-    vector = []
+    Q=0.1 #Q = Sigma
+    g=0 #generacion
+    E=0
     tam=int(raw_input("Ingrese el tamano del vector:\n"))
     print "Ingrese el intervalo\n:"
     min = int(raw_input("min:\n"))
     max= int(raw_input("max:\n"))
     #Generar numeros aleatorios del tamaño tam,
-    ale=Sol_ale(min,max,tam,vector)
+    ale=Sol_ale(min,max,tam)
     #Evaluar.
-    g=0
-    E=0
-
-    evaluado=Evaluar(ale)
-    print "Generacion:"+str(g)+"\tVector:"+str(ale)+"\tAptitud:"+str(evaluado)+"\tExitos:"+str(E)
+    eo=Evaluar(ale) #evaluacion inicial
+    imprimir(g,ale,eo,E)
     raw_input("Espera")
     #Establecer Q
-    n=N(tam,Q)
-    xi=Mutar(ale,n)
-    print "Elemento mutado:\n"
-    print str(xi)
+    while g!=100:
+        n=N(tam,Q)
+        xi=Mutar(ale,n)
+        ei = Evaluar(xi)
+        E,eo,x=comparar(eo,ei,E,xi,ale)
+        g=g+1
+        imprimir(g,x,ei,E)
+        if E == 10:
+            #Modificar Sigma.
+            Q=Modificar_Sigma()
+            E=0
 
-    print "Evaluar Hijo\n"
-    evalhijo = Evaluar(xi)
-    print str(evalhijo)
+
+ #   while e!=10:
+
+
 
 
 
